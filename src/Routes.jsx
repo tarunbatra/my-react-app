c37e86c3251509d1a24465a8cbadc4b075b0252a
Tarun Batra <tarun.batra00@gmail.com>
2017-02-14 12:35:18 +0530

    Fixes eslint errors

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,34 +1,24 @@
 import React from 'react';
-import { Router, Route, IndexRoute, Link, Redirect, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router';
 
-import NotFoundComponent from 'modules/NotFound';
+import NotFoundComponent from './modules/NotFound';
 
-class Root extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className = "RootTitle">
-          Welcome to my-react-app
-        </h1>
-      </div>
-    );
-  }
-};
+const Root = () =>
+  <div>
+    <h1 className="RootTitle">
+      Welcome to my-react-app
+    </h1>
+  </div>;
 
-class Routes extends React.Component {
-  render() {
-    return (
-      <Router history = { browserHistory }>
-        <Route path = '/'>
-          <IndexRoute component={ Root } />
-        </Route>
+const Routes = () =>
+  <Router history={browserHistory}>
+    <Route path="/">
+      <IndexRoute component={Root} />
+    </Route>
 
-        { /* 404 Route */ }
-        <Route path = '/404' component = { NotFoundComponent } />
-        <Redirect from='*' to='/404' />
-      </Router>
-    )
-  }
-}
+    { /* 404 Route */ }
+    <Route path="/404" component={NotFoundComponent} />
+    <Redirect from="*" to="/404" />
+  </Router>;
 
 export default Routes;
